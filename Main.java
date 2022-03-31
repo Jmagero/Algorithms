75054ccd4dd23375498ec10eb8ee00a667bb0b57
Jocyline Magero <52098394+Jmagero@users.noreply.github.com>
2022-03-31 14:35:18 +0000

    Add insertion sort function

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     int[] arr = {5,8,1,3,4};
-    sort(arr);
+    insertionSort(arr);
     printArray(arr);
   }
 
-  public static int[] sort(int[] arr){
+  public static int[] selectionSort(int[] arr){
     for(int i=0; i<arr.length; i++){
       int min = arr[i];
       for(int j = i+1; j < arr.length; j++){
@@ -23,6 +23,21 @@ class Main {
     return arr;
 
   }
+
+  public static int[] insertionSort(int[] arr){
+    for(int i = 0; i < arr.length; i++){
+      int temp = arr[i];
+      int j = i-1;
+      while (j >= 0 && arr[j] > temp){
+        arr[j+1] = arr[j];
+        j--;
+      }
+      arr[j+1] = temp;
+    }
+    return arr;
+  }
+
+  
   public static void printArray(int arr[])
     {
         int n = arr.length;
